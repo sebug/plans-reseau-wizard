e5539a9ad7a28cb34385bdfdab719a789069c3e3
sebug <sebastian.gfeller@gmail.com>
2017-10-16 18:28:41 +0200

    Table input, maybe?

--- WizardEntryTrigger/index.js
+++ WizardEntryTrigger/index.js
@@ -6,7 +6,7 @@ module.exports = function (context, req) {
     if (req.query.name || (req.body && req.body.name)) {
         context.res = {
             // status: 200, /* Defaults to 200 */
-            body: "Ohai " + (req.query.name || req.body.name)
+            body: "Ohai " + context.bindings.settings
         };
     }
     else {
