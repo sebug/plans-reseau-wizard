01abce7c5942b71fd341da5b9c0d799466844171
sebug <sebastian.gfeller@gmail.com>
2017-10-16 18:39:59 +0200

    Only App ID

--- WizardEntryTrigger/index.js
+++ WizardEntryTrigger/index.js
@@ -1,10 +1,8 @@
 module.exports = function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
-    var staticUrl = 'http://storageplansreseau.blob.core.windows.net/'
-
     context.res = {
-        body: "Ohai " + context.bindings.settings.Value + staticUrl
+        body: String(context.bindings.settings.Value)
     };
     context.done();
 };
