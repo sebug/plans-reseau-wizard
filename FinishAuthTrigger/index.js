0df4a13f168fb27cc0b54311e306afb160173922
sebug <sebastian.gfeller@gmail.com>
2017-10-16 19:53:29 +0200

    Doing finish authenticate call

--- FinishAuthTrigger/index.js
+++ FinishAuthTrigger/index.js
@@ -1,3 +1,5 @@
+var request = require('request');
+
 module.exports = function (context, req) {
     context.log('JavaScript HTTP trigger for finish auth. ' +
 		context.bindings.settings.Value + " " +
@@ -5,10 +7,26 @@ module.exports = function (context, req) {
 		(req.query.code || req.body.code) + " " +
 		(req.query.redirectUri || req.body.redirectUri));
 
-    context.res = {
-        body: {
-	    appID: String(context.bindings.settings.Value)
+    request({
+	method: 'POST',
+	uri: 'https://login.microsoftonline.com/common/oauth2/v2.0/token',
+	form: {
+	    client_id: context.bindings.settings.Value,
+	    redirect_uri: (req.query.redirectUri || req.body.redirectUri),
+	    client_secret: context.bindings.secret.Value,
+	    code: (req.query.code || req.body.code)
 	}
-    };
+    }, function (error, response, body) {
+	if (response.statusCode == 200) {
+	    context.res = {
+		body: body
+	    };
+	} else {
+	    context.res = {
+		status: response.statusCode,
+		body: 'error: ' + body
+	    };
+	}
+    });
     context.done();
 };
