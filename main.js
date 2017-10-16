68c476b6943864d7b702ce85fdd91f3c0b7f222b
sebug <sebastian.gfeller@gmail.com>
2017-10-16 11:07:33 +0200

    jQuery from CDN

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
+/* global $ */
 (function () {
-    console.log('Test');
+    var d = $.Deferred();
+    d.resolve(true);
+    d.promise().then(function () {
+	console.log('Test 2');
+    });
 }());
