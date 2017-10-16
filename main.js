23ddb2e93e185d216331e174d3b9660974ac8ea5
sebug <sebastian.gfeller@gmail.com>
2017-10-16 14:03:56 +0200

    Ready for template upload

--- main.js
+++ main.js
@@ -1,7 +1,16 @@
-/* global $, ko */
+/* global $, ko, FormData */
 (function () {
     var viewModel = {
-	message: ko.observable('Oh, hey')
+	templateFile: ko.observable(),
+	analyzeTemplate: function(file) {
+	    if (file) {
+		var formData = new FormData();
+		formData.append("template", file);
+		var request = new XMLHttpRequest();
+		request.open("POST",".");
+		request.send(formData);
+	    }
+	}
     };
 
     function fetchWizardEntryData() {
@@ -9,7 +18,7 @@
 	    url: 'http://PlansReseau.azurewebsites.net/api/WizardEntryTrigger?name=Sebastian',
 	    dataType: 'json'
 	}).then(function (msg) {
-	    viewModel.message(msg);
+	    console.log(msg);
 	});
     }
     
