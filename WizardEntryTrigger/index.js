11487d7a1abae53c927e78e815af263ea8d19b5b
sebug <sebastian.gfeller@gmail.com>
2017-10-16 18:36:23 +0200

    forget about name

--- WizardEntryTrigger/index.js
+++ WizardEntryTrigger/index.js
@@ -3,17 +3,9 @@ module.exports = function (context, req) {
 
     var staticUrl = 'http://storageplansreseau.blob.core.windows.net/'
 
-    if (req.query.name || (req.body && req.body.name)) {
-        context.res = {
-            // status: 200, /* Defaults to 200 */
-            body: "Ohai " + context.bindings.settings
-        };
-    }
-    else {
-        context.res = {
-            status: 400,
-            body: "Please pass a name on the query string or in the request body"
-        };
-    }
+    context.res = {
+        // status: 200, /* Defaults to 200 */
+        body: "Ohai " + context.bindings.settings + staticUrl;
+    };
     context.done();
 };
