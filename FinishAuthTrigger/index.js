b596736a07348cbdc9a16bf197484d407070ddce
sebug <sebastian.gfeller@gmail.com>
2017-10-16 19:27:27 +0200

    Directly from req

--- FinishAuthTrigger/index.js
+++ FinishAuthTrigger/index.js
@@ -2,7 +2,7 @@ module.exports = function (context, req) {
     context.log('JavaScript HTTP trigger for finish auth. ' +
 		context.bindings.settings.Value + " " +
 		context.bindings.secret + " " +
-	       context.req.body.code);
+	       req.body.code);
 
     context.res = {
         body: {
